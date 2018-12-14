1a67cc7abeb8aea7c8465dfb26477d7dbb30a779
gleds3000 <gleds3000@gmail.com>
2018-12-14 17:16:38 -0200

    meu primeiro php

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+
+define('DB_SERVER', 'demo.c7qbcuicfwpb.us-east-2.rds.amazonaws.com');
+define('DB_NAME', 'demo');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'devopsminda');
+
+$link = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
+
+if($link === false){
+    die("Erro: Nao foi possivel conectar : ". mysqli_connect_error() );
+    echo $link;
+}
+
+?>
---
+++ create.php
@@ -0,0 +1,110 @@
+<?php
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$name = $address = $salary = "";
+$name_err = $address_err = $salary_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    // Validate name
+    $input_name = trim($_POST["name"]);
+    if(empty($input_name)){
+        $name_err = "Informe um nome.";
+    } elseif(!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $name_err = "informe um nome valido.";
+    } else{
+        $name = $input_name;
+    }
+    
+    // Validate address
+    $input_address = trim($_POST["address"]);
+    if(empty($input_address)){
+        $address_err = "Informe um endereco.";     
+    } else{
+        $address = $input_address;
+    }
+    
+    // Validate salary
+    $input_salary = trim($_POST["salary"]);
+    if(empty($input_salary)){
+        $salary_err = "Informe um valor de salario cheio sem decimal.";     
+    } elseif(!ctype_digit($input_salary)){
+        $salary_err = "Informe um valor positivo e inteiro ex: 1000.";
+    } else{
+        $salary = $input_salary;
+    }
+    if(empty($name_err) && empty($address_err) && empty($salary_err)){
+    
+        $sql = "INSERT INTO employees (name, address, salary) VALUES (?, ?, ?)";
+         
+        if($stmt = mysqli_prepare($link, $sql)){
+            mysqli_stmt_bind_param($stmt, "sss", $param_name, $param_address, $param_salary);
+            
+            $param_name = $name;
+            $param_address = $address;
+            $param_salary = $salary;
+            
+            if(mysqli_stmt_execute($stmt)){
+                header("location: index.php");
+                exit();
+            } else{
+                echo "Alguma coisa não está bem tente novamente ou avise o adm do sistema";
+            }
+        }
+         
+        mysqli_stmt_close($stmt);
+    }
+    
+    mysqli_close($link);
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Create Record</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        .wrapper{
+            width: 500px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header">
+                        <h2>Novo Funcionário</h2>
+                    </div>
+                    <p>Preencha os campos </p>
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <div class="form-group <?php echo (!empty($name_err)) ? 'has-error' : ''; ?>">
+                            <label>Nome</label>
+                            <input type="text" name="name" class="form-control" value="<?php echo $name; ?>">
+                            <span class="help-block"><?php echo $name_err;?></span>
+                        </div>
+                        <div class="form-group <?php echo (!empty($address_err)) ? 'has-error' : ''; ?>">
+                            <label>Endereço</label>
+                            <textarea name="address" class="form-control"><?php echo $address; ?></textarea>
+                            <span class="help-block"><?php echo $address_err;?></span>
+                        </div>
+                        <div class="form-group <?php echo (!empty($salary_err)) ? 'has-error' : ''; ?>">
+                            <label>Salário</label>
+                            <input type="text" name="salary" class="form-control" value="<?php echo $salary; ?>">
+                            <span class="help-block"><?php echo $salary_err;?></span>
+                        </div>
+                        <input type="submit" class="btn btn-primary" value="Enviar">
+                        <a href="index.php" class="btn btn-default">Cancelar</a>
+                    </form>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
